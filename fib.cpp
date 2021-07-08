9eebc8c19296fe47919ba31addb549aa44be28a6
AntufeevMaksim <87116331+AntufeevMaksim@users.noreply.github.com>
2021-07-08 14:56:04 +0700

    Update and rename fib to fib.cpp

---
+++ fib.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+
+
+int main(){
+    int up_to;
+    std::cin >> up_to;
+
+    int fib1 = 0;
+    int fib2 = 1;
+    int f;
+    printf("%d", fib1);
+
+    for (int i = 0; i<=up_to; i++){
+        printf("%d ", fib2);
+
+        f = fib1;
+        fib1 = fib2;
+        fib2 += f;
+
+
+    }
+    return 0;
+
+}
